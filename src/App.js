d5edd4482497edd01cc2461154e7ac741b2fbb4a
Michael J. Galasso <sales@thecanonfather.com>
2025-06-02 15:59:28 -0400

    🧵 Added DIGUM title component, branding, and canonical upgrades

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import MirrorGatewayAlpha from "./components/MirrorGatewayAlpha";
+import React from 'react';
+import MirrorGatewayAlpha from './components/MirrorGatewayAlpha';
+import DigumTitle from './components/DigumTitle';
+import './components/DigumTitle.css';
 
 function App() {
-  return <MirrorGatewayAlpha />;
+  return (
+    <div className="App">
+      <DigumTitle />
+      <MirrorGatewayAlpha />
+    </div>
+  );
 }
 
 export default App;
