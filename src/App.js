aab4d64f754c604f39a3f0ba31f19bd1f10e9d52
Michael J. Galasso <sales@thecanonfather.com>
2025-06-02 22:28:54 -0400

    🧵 Added sos.png background to shrine

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Header from './components/Header';
 import React from 'react';
 import MirrorGatewayAlpha from './components/MirrorGatewayAlpha';
 import DigumTitle from './components/DigumTitle';
+import KioskBackground from './components/KioskBackground';
+
 function App() {
   return (
     <div className="App">
-     <Header />
-     <DigumTitle />
-     <MirrorGatewayAlpha />
+      <KioskBackground />
+      <Header />
+      <DigumTitle />
+      <MirrorGatewayAlpha />
     </div>
   );
 }
---
+++ src/components/KioskBackground.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import sos from '../assets/sos.png'; // or adjust path
+
+const style = {
+  backgroundImage: `url(${sos})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  width: '100vw',
+  height: '100vh',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: -1
+};
+
+export default function KioskBackground() {
+  return <div style={style}></div>;
+}
