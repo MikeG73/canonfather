7eb8cbc62f439ed6cc73e33a8aaee5724f4638ca
Michael J. Galasso <sales@thecanonfather.com>
2025-06-03 12:48:40 -0400

    🚀 Canon Engine build ready for deployment..again1

--- src/components/BlockCounter.jsx
+++ src/components/BlockCounter.jsx
@@ -14,11 +14,18 @@ export default function BlockCounter() {
   return (
     <div className="block-reactor">
       <div className="block-flare" />
+      
       <p className="block-label">CANON ENGINE BLOCK COUNT</p>
+      
       <div className="block-readout">
         <span className="block-hash"># {block}</span>
       </div>
-      <img src={require('../assets/sos.png')} alt="SOS Burn" className="sos-burn" />
+      
+      <img
+        src={require('../assets/sos.png')}
+        alt="SOS Burn"
+        className="sos-burn"
+      />
     </div>
   );
 }
