a7147d44eb69d92323a63df667d142c80f321bad
Michael J. Galasso <sales@thecanonfather.com>
2025-06-07 11:56:16 -0400

    "Modified KioskBackground.jsx"

--- src/components/KioskBackground.jsx
+++ src/components/KioskBackground.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-const style = {
-  backgroundImage: "url('/sos.png')",
-  backgroundSize: 'cover',
-  backgroundPosition: 'center center',
-  backgroundRepeat: 'no-repeat',
-  width: '100vw',
-  height: '100vh',
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  zIndex: -1
-};
-
 export default function KioskBackground() {
-  return <div style={style}></div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url("${process.env.PUBLIC_URL}/sos.png")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    />
+  );
 }
